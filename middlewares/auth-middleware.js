16ec84805149256f0acefce5c4a2001ba687d03f
JangJaeWon22 <nic2866@naver.com>
2021-09-30 23:33:30 +0900

    회원가입 api, 로그인 api, 굿즈 crud 가져오기 완료

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,30 @@
+const jwt =require('jsonwebtoken');
+const User = require('../models/user')
+
+module.exports = (req, res, next) => {
+    // console.log("auth미들웨어 사용함")
+    const { authorization } = req.headers;
+    // console.log(authorization); 
+    const [tokenType, tokenValue] = authorization.split(' ') // authorization에 split 속성을 줘서 Bearer 과 JW토큰내용을 구분
+    
+    if (tokenType !== "Bearer"){
+        res.status(401).send({
+            errorMessage: "로그인 후 사용하세요",
+        });
+        return;
+    }
+
+    try{
+        const { userId } = jwt.verify(tokenValue, "1q2w3e4r1!");
+        User.findById(userId).exec().then((user) => {
+            //핵심!!! locals!!
+            res.locals.user = user; // 중요!!!
+            next(); // next가 반드시 호출 되어야 됨.!!
+        });
+    }catch (error){
+        res.status(401).send({
+            errorMessage: "로그인 후 사용하세요",
+        });
+        return;
+    }
+};
